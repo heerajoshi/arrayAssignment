b165258df8bff83d0da50c3047219e6e1f53b7dd
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2018-10-17 21:30:54 +0530

    Add a shell script
    This script check all the test

--- tests/ascending_order_test.js
+++ tests/ascending_order_test.js
@@ -3,22 +3,22 @@ let isAscendingOrder = require("../library/array_library.js").isAscendingOrder;
 
 
 let input = [1,6,8,5,9,2];
-let expectedOutput = "false";
+let expectedOutput = false;
 let actualOutput = isAscendingOrder(input);
     assert.deepEqual(actualOutput , expectedOutput);
 
 let input2 = [2,3,6,5];
-let expectedOutput2 = "false";
+let expectedOutput2 = false;
 let actualOutput2 = isAscendingOrder(input2);
     assert.deepEqual(actualOutput2 , expectedOutput2);
 
 let input3 = [1,2,4,5,7];
-let expectedOutput3 = "true";
+let expectedOutput3 = true;
 let actualOutput3 = isAscendingOrder(input3);
     assert.deepEqual(actualOutput3 , expectedOutput3);
 
 let input4 = [2,6,9];
-let expectedOutput4 = "true";
+let expectedOutput4 = true;
 let actualOutput4 = isAscendingOrder(input4);
     assert.deepEqual(actualOutput4 , expectedOutput4);
 
