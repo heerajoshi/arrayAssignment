4571059b6d8420f926bdacf5ca909b74bc5ff8c0
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2018-10-20 00:32:51 +0530

    Using for loop for test files in shell script
    one loop to access all test file

--- all_array_test.sh
+++ all_array_test.sh
@@ -1,28 +1,14 @@
-#./tests/bi./tests/bash
-node ./tests/ascending_order.js
-node ./tests/count_number__below.js
-node ./tests/lowest_element_test.js
-node ./tests/testForslectEven.js
-node ./tests/ascending_order_test.js
-node ./tests/count_odd_array_test.js
-node ./tests/mapping_length_test.js
-node ./tests/testForslectOdd.js
-node ./tests/average_of_element_test.js
-node ./tests/descending_order_test.js
-node ./tests/partition_test.js
-node ./tests/testForsumOfArray.js
-node ./tests/checkSubset.js
-node ./tests/differenceOfArray.js
-node ./tests/printing_reverse_test.js
-node ./tests/unique_element_test.js
-node ./tests/concat_two_array_test.js
-node ./tests/exterect_test.js
-node ./tests/reverse_fibo_test.js
-node ./tests/zipArryTest.js
-node ./tests/count_Even_array_test.js
-node ./tests/greatest_element_test.js
-node ./tests/rotate_array_test.js
-node ./tests/count_number__above.js
-node ./tests/intersection_of_array_test.js
-node ./tests/select_second_element_test.js
-echo "================All test passed==============="
+#! /bin/bash
+all_test_dir="./tests/"
+all_test_files=$(ls $all_test_dir)
+
+
+
+for filename in $all_test_files
+
+do 
+  node $all_test_dir/$all_test_files
+
+  echo "=============="$filename "passed"==============
+
+done
